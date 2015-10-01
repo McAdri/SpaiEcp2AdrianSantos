c54e39864fc2003d3bb30932d48228fa3064c184
Jorgelou <sirjorgete@gmail.com>
2015-10-01 21:30:29 +0200

    Point

--- src/main/java/es/upm/miw/iwvg/junit/Point.java
+++ src/main/java/es/upm/miw/iwvg/junit/Point.java
@@ -46,5 +46,13 @@ public class Point {
     public Point puntoMedio(Point point) {
     	return new Point((this.x + point.x)/2, (this.y + point.y)/2);
     }
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
     
 }
